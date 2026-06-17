c9592c8f185df2a22f7179881fb4b8614138674c
Alexis Osmond <aosmond@umich.edu>
2026-06-17 18:11:03 -0400

    syncfile changed back

--- src/syncfile.cpp
+++ src/syncfile.cpp
@@ -33,27 +33,27 @@ bool SyncFile::write(const std::string& data) {
   }
 }
 
-// bool SyncFile::write(const csv_data& data) {
-//   try {
-//     // Get the file lock for this thread
-//     std::lock_guard<std::mutex> lock(_writerMutex);
-//     // put data into a queue to be written later
-//     _writeQueue.push(data);
-//     //_csv_output << data << std::endl;
-//     return true;
-//   } catch (const std::exception& e) {
-//     std::cerr << e.what() << '\n';
-//     return false;
-//   }
-// }
-
 bool SyncFile::write(const csv_data& data) {
   try {
+    // Get the file lock for this thread
     std::lock_guard<std::mutex> lock(_writerMutex);
-    _csv_output << data << '\n';
+    // put data into a queue to be written later
+    _writeQueue.push(data);
+    //_csv_output << data << std::endl;
     return true;
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
     return false;
   }
 }
+
+// bool SyncFile::write(const csv_data& data) {
+//   try {
+//     std::lock_guard<std::mutex> lock(_writerMutex);
+//     _csv_output << data << '\n';
+//     return true;
+//   } catch (const std::exception& e) {
+//     std::cerr << e.what() << '\n';
+//     return false;
+//   }
+// }
