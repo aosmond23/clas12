1e6efa134b8c059d8ec3be450eec70e4c2701b22
Alexis Osmond <aosmond@umich.edu>
2026-08-28 09:33:07 -0400

    same

--- src/cuts.cpp
+++ src/cuts.cpp
@@ -218,112 +218,112 @@ bool Pass2_Cuts::ElectronCuts() {
 // }
 
 // bool Pass2_Cuts::DC_fiducial_cut_XY(int i, int pid)
-{
-        bool _dc_fid_cut = true;
-        /// supergaus
-        // use minparams_in and maxparams_in for Y limits of a valid hit in given X; gives Y range for specific X
-        // min_r for inner circular cut (min radius cut)
-        double minparams_in[3][6][3][2] = {
-            {{{-0.58933, 15.03000}, {-0.60189, 22.39868}, {-0.54181, 21.96126}},
-             {{-0.58242, 14.04545}, {-0.59240, 20.85846}, {-0.55192, 22.29423}},
-             {{-0.55927, 14.26636}, {-0.57437, 21.95077}, {-0.51302, 21.37775}},
-             {{-0.56533, 14.35000}, {-0.57253, 21.57033}, {-0.52352, 22.35879}},
-             {{-0.58582, 16.10909}, {-0.58703, 23.30440}, {-0.52440, 23.26868}},
-             {{-0.57624, 13.84455}, {-0.59262, 21.42824}, {-0.53071, 20.85082}}},
+// {
+//         bool _dc_fid_cut = true;
+//         /// supergaus
+//         // use minparams_in and maxparams_in for Y limits of a valid hit in given X; gives Y range for specific X
+//         // min_r for inner circular cut (min radius cut)
+//         double minparams_in[3][6][3][2] = {
+//             {{{-0.58933, 15.03000}, {-0.60189, 22.39868}, {-0.54181, 21.96126}},
+//              {{-0.58242, 14.04545}, {-0.59240, 20.85846}, {-0.55192, 22.29423}},
+//              {{-0.55927, 14.26636}, {-0.57437, 21.95077}, {-0.51302, 21.37775}},
+//              {{-0.56533, 14.35000}, {-0.57253, 21.57033}, {-0.52352, 22.35879}},
+//              {{-0.58582, 16.10909}, {-0.58703, 23.30440}, {-0.52440, 23.26868}},
+//              {{-0.57624, 13.84455}, {-0.59262, 21.42824}, {-0.53071, 20.85082}}},
 
-            {{{-0.51812, 4.80882}, {-0.60660, 19.32861}, {-0.60392, 34.35024}},
-             {{-0.52912, 5.39632}, {-0.61995, 20.57325}, {-0.62008, 37.14595}},
-             {{-0.50012, 4.10882}, {-0.59236, 18.29823}, {-0.58771, 32.53500}},
-             {{-0.52647, 5.16029}, {-0.60426, 18.88900}, {-0.60434, 34.83024}},
-             {{-0.50600, 4.00000}, {-0.58717, 17.29909}, {-0.57177, 28.34119}},
-             {{-0.52388, 4.91618}, {-0.60842, 19.32450}, {-0.61696, 36.42214}}},
+//             {{{-0.51812, 4.80882}, {-0.60660, 19.32861}, {-0.60392, 34.35024}},
+//              {{-0.52912, 5.39632}, {-0.61995, 20.57325}, {-0.62008, 37.14595}},
+//              {{-0.50012, 4.10882}, {-0.59236, 18.29823}, {-0.58771, 32.53500}},
+//              {{-0.52647, 5.16029}, {-0.60426, 18.88900}, {-0.60434, 34.83024}},
+//              {{-0.50600, 4.00000}, {-0.58717, 17.29909}, {-0.57177, 28.34119}},
+//              {{-0.52388, 4.91618}, {-0.60842, 19.32450}, {-0.61696, 36.42214}}},
 
-            {{{-0.48371, 2.71544}, {-0.56539, 14.80074}, {-0.56644, 28.90231}},
-             {{-0.49800, 3.45000}, {-0.56931, 14.84191}, {-0.56402, 27.89462}},
-             {{-0.49165, 3.19853}, {-0.55265, 13.67279}, {-0.56789, 30.39264}},
-             {{-0.48529, 2.72206}, {-0.56480, 14.85221}, {-0.57158, 30.11088}},
-             {{-0.46906, 1.67941}, {-0.55137, 13.52941}, {-0.54323, 24.85703}},
-             {{-0.51318, 4.11324}, {-0.56931, 14.84191}, {-0.56697, 28.84165}}}
+//             {{{-0.48371, 2.71544}, {-0.56539, 14.80074}, {-0.56644, 28.90231}},
+//              {{-0.49800, 3.45000}, {-0.56931, 14.84191}, {-0.56402, 27.89462}},
+//              {{-0.49165, 3.19853}, {-0.55265, 13.67279}, {-0.56789, 30.39264}},
+//              {{-0.48529, 2.72206}, {-0.56480, 14.85221}, {-0.57158, 30.11088}},
+//              {{-0.46906, 1.67941}, {-0.55137, 13.52941}, {-0.54323, 24.85703}},
+//              {{-0.51318, 4.11324}, {-0.56931, 14.84191}, {-0.56697, 28.84165}}}
 
-        };
+//         };
 
-        double maxparams_in[3][6][3][2] = {
-            {{{0.57079, -13.46727}, {0.57108, -19.36121}, {0.52978, -20.38791}},
-             {{0.56255, -14.79273}, {0.58448, -23.36066}, {0.52522, -23.29478}},
-             {{0.62473, -17.85364}, {0.59042, -23.65901}, {0.53396, -24.24066}},
-             {{0.58242, -14.04545}, {0.59934, -21.59780}, {0.55005, -22.42995}},
-             {{0.57624, -15.64455}, {0.59525, -23.87989}, {0.53313, -24.02225}},
-             {{0.59309, -15.06545}, {0.60044, -22.17527}, {0.54753, -22.55247}}},
+//         double maxparams_in[3][6][3][2] = {
+//             {{{0.57079, -13.46727}, {0.57108, -19.36121}, {0.52978, -20.38791}},
+//              {{0.56255, -14.79273}, {0.58448, -23.36066}, {0.52522, -23.29478}},
+//              {{0.62473, -17.85364}, {0.59042, -23.65901}, {0.53396, -24.24066}},
+//              {{0.58242, -14.04545}, {0.59934, -21.59780}, {0.55005, -22.42995}},
+//              {{0.57624, -15.64455}, {0.59525, -23.87989}, {0.53313, -24.02225}},
+//              {{0.59309, -15.06545}, {0.60044, -22.17527}, {0.54753, -22.55247}}},
 
-            {{{0.53865, -5.86103}, {0.61408, -19.00965}, {0.64226, -38.73976}},
-             {{0.52876, -5.36985}, {0.61309, -19.36563}, {0.61519, -34.45357}},
-             {{0.52388, -4.91618}, {0.60462, -18.49771}, {0.61353, -34.83833}},
-             {{0.53606, -5.61691}, {0.61106, -18.60558}, {0.62413, -35.58310}},
-             {{0.51347, -4.49779}, {0.60016, -18.02074}, {0.59738, -31.99500}},
-             {{0.52918, -5.16324}, {0.60592, -18.18320}, {0.61468, -33.58214}}},
+//             {{{0.53865, -5.86103}, {0.61408, -19.00965}, {0.64226, -38.73976}},
+//              {{0.52876, -5.36985}, {0.61309, -19.36563}, {0.61519, -34.45357}},
+//              {{0.52388, -4.91618}, {0.60462, -18.49771}, {0.61353, -34.83833}},
+//              {{0.53606, -5.61691}, {0.61106, -18.60558}, {0.62413, -35.58310}},
+//              {{0.51347, -4.49779}, {0.60016, -18.02074}, {0.59738, -31.99500}},
+//              {{0.52918, -5.16324}, {0.60592, -18.18320}, {0.61468, -33.58214}}},
 
-            {{{0.49447, -3.13529}, {0.55402, -12.47426}, {0.57062, -26.22209}},
-             {{0.49800, -3.45000}, {0.56431, -13.96176}, {0.57756, -28.62341}},
-             {{0.49235, -2.92647}, {0.55402, -12.61544}, {0.56486, -26.74143}},
-             {{0.49818, -3.30074}, {0.56431, -13.45588}, {0.56947, -26.46780}},
-             {{0.48971, -2.89044}, {0.55402, -12.61544}, {0.55198, -24.45110}},
-             {{0.49818, -3.30074}, {0.56431, -13.45588}, {0.56565, -25.70758}}}};
+//             {{{0.49447, -3.13529}, {0.55402, -12.47426}, {0.57062, -26.22209}},
+//              {{0.49800, -3.45000}, {0.56431, -13.96176}, {0.57756, -28.62341}},
+//              {{0.49235, -2.92647}, {0.55402, -12.61544}, {0.56486, -26.74143}},
+//              {{0.49818, -3.30074}, {0.56431, -13.45588}, {0.56947, -26.46780}},
+//              {{0.48971, -2.89044}, {0.55402, -12.61544}, {0.55198, -24.45110}},
+//              {{0.49818, -3.30074}, {0.56431, -13.45588}, {0.56565, -25.70758}}}};
 
-        double min_r[3][6] = {{35.775, 35.765, 35.787, 36.065, 35.887, 36.104},
-                              {53.531, 53.314, 52.660, 53.439, 53.473, 53.412},
-                              {62.403, 63.254, 63.941, 63.396, 63.273, 62.715}};
+//         double min_r[3][6] = {{35.775, 35.765, 35.787, 36.065, 35.887, 36.104},
+//                               {53.531, 53.314, 52.660, 53.439, 53.473, 53.412},
+//                               {62.403, 63.254, 63.941, 63.396, 63.273, 62.715}};
 
-        // identify sector where particle was detected
-        // short dc_sector = (_data->dc_sec(i) - 1);
+//         // identify sector where particle was detected
+//         // short dc_sector = (_data->dc_sec(i) - 1);
 
-        int raw_sector = _data->dc_sec(i);
+//         int raw_sector = _data->dc_sec(i);
 
-        if (raw_sector <= 0 || raw_sector > 6) {
-            return false;
-        }
+//         if (raw_sector <= 0 || raw_sector > 6) {
+//             return false;
+//         }
 
-        short dc_sector = raw_sector - 1;
+//         short dc_sector = raw_sector - 1;
 
-        // region 1
-        double X1 = _data->dc_r1_x(i);
-        double Y1 = _data->dc_r1_y(i);
-        float X1_new = X1 * cos(DEG2RAD * (-60 * (dc_sector))) - Y1 * sin(DEG2RAD * (-60 * (dc_sector)));
-        Y1 = X1 * sin(DEG2RAD * (-60 * (dc_sector))) + Y1 * cos(DEG2RAD * (-60 * (dc_sector)));
+//         // region 1
+//         double X1 = _data->dc_r1_x(i);
+//         double Y1 = _data->dc_r1_y(i);
+//         float X1_new = X1 * cos(DEG2RAD * (-60 * (dc_sector))) - Y1 * sin(DEG2RAD * (-60 * (dc_sector)));
+//         Y1 = X1 * sin(DEG2RAD * (-60 * (dc_sector))) + Y1 * cos(DEG2RAD * (-60 * (dc_sector)));
 
-        X1 = X1_new;
-        int region_1 = 1;
+//         X1 = X1_new;
+//         int region_1 = 1;
 
-        double calc_min1 = minparams_in[pid][dc_sector][region_1 - 1][0] * X1 + minparams_in[pid][dc_sector][region_1 - 1][1];
-        double calc_max1 = maxparams_in[pid][dc_sector][region_1 - 1][0] * X1 + maxparams_in[pid][dc_sector][region_1 - 1][1];
-        double DC_r1 = sqrt(X1 * X1 + Y1 * Y1);
-        double Min_r1 = min_r[region_1 - 1][dc_sector];
-        // region 2
-        double X2 = _data->dc_r2_x(i);
-        double Y2 = _data->dc_r2_y(i);
-        float X2_new = X2 * cos(DEG2RAD * (-60 * (dc_sector))) - Y2 * sin(DEG2RAD * (-60 * (dc_sector)));
-        Y2 = X2 * sin(DEG2RAD * (-60 * (dc_sector))) + Y2 * cos(DEG2RAD * (-60 * (dc_sector)));
-        X2 = X2_new;
-        int region_2 = 2;
-        double calc_min2 = minparams_in[pid][dc_sector][region_2 - 1][0] * X2 + minparams_in[pid][dc_sector][region_2 - 1][1];
-        double calc_max2 = maxparams_in[pid][dc_sector][region_2 - 1][0] * X2 + maxparams_in[pid][dc_sector][region_2 - 1][1];
-        double DC_r2 = sqrt(X2 * X2 + Y2 * Y2);
-        double Min_r2 = min_r[region_2 - 1][dc_sector];
-        // region 3
-        double X3 = _data->dc_r3_x(i);
-        double Y3 = _data->dc_r3_y(i);
-        float X3_new = X3 * cos(DEG2RAD * (-60 * (dc_sector))) - Y3 * sin(DEG2RAD * (-60 * (dc_sector)));
-        Y3 = X3 * sin(DEG2RAD * (-60 * (dc_sector))) + Y3 * cos(DEG2RAD * (-60 * (dc_sector)));
-        X3 = X3_new;
-        int region_3 = 3;
-        double calc_min3 = minparams_in[pid][dc_sector][region_3 - 1][0] * X3 + minparams_in[pid][dc_sector][region_3 - 1][1];
-        double calc_max3 = maxparams_in[pid][dc_sector][region_3 - 1][0] * X3 + maxparams_in[pid][dc_sector][region_3 - 1][1];
-        double DC_r3 = sqrt(X3 * X3 + Y3 * Y3);
-        double Min_r3 = min_r[region_3 - 1][dc_sector];
+//         double calc_min1 = minparams_in[pid][dc_sector][region_1 - 1][0] * X1 + minparams_in[pid][dc_sector][region_1 - 1][1];
+//         double calc_max1 = maxparams_in[pid][dc_sector][region_1 - 1][0] * X1 + maxparams_in[pid][dc_sector][region_1 - 1][1];
+//         double DC_r1 = sqrt(X1 * X1 + Y1 * Y1);
+//         double Min_r1 = min_r[region_1 - 1][dc_sector];
+//         // region 2
+//         double X2 = _data->dc_r2_x(i);
+//         double Y2 = _data->dc_r2_y(i);
+//         float X2_new = X2 * cos(DEG2RAD * (-60 * (dc_sector))) - Y2 * sin(DEG2RAD * (-60 * (dc_sector)));
+//         Y2 = X2 * sin(DEG2RAD * (-60 * (dc_sector))) + Y2 * cos(DEG2RAD * (-60 * (dc_sector)));
+//         X2 = X2_new;
+//         int region_2 = 2;
+//         double calc_min2 = minparams_in[pid][dc_sector][region_2 - 1][0] * X2 + minparams_in[pid][dc_sector][region_2 - 1][1];
+//         double calc_max2 = maxparams_in[pid][dc_sector][region_2 - 1][0] * X2 + maxparams_in[pid][dc_sector][region_2 - 1][1];
+//         double DC_r2 = sqrt(X2 * X2 + Y2 * Y2);
+//         double Min_r2 = min_r[region_2 - 1][dc_sector];
+//         // region 3
+//         double X3 = _data->dc_r3_x(i);
+//         double Y3 = _data->dc_r3_y(i);
+//         float X3_new = X3 * cos(DEG2RAD * (-60 * (dc_sector))) - Y3 * sin(DEG2RAD * (-60 * (dc_sector)));
+//         Y3 = X3 * sin(DEG2RAD * (-60 * (dc_sector))) + Y3 * cos(DEG2RAD * (-60 * (dc_sector)));
+//         X3 = X3_new;
+//         int region_3 = 3;
+//         double calc_min3 = minparams_in[pid][dc_sector][region_3 - 1][0] * X3 + minparams_in[pid][dc_sector][region_3 - 1][1];
+//         double calc_max3 = maxparams_in[pid][dc_sector][region_3 - 1][0] * X3 + maxparams_in[pid][dc_sector][region_3 - 1][1];
+//         double DC_r3 = sqrt(X3 * X3 + Y3 * Y3);
+//         double Min_r3 = min_r[region_3 - 1][dc_sector];
 
-        // particle must be within all fiducial bands in all 3 regions and pass inner circular cut
-        return ((Y1 > calc_min1) && (Y1 < calc_max1) && (Y2 > calc_min2) && (Y2 < calc_max2) && (Y3 > calc_min3) &&
-                (Y3 < calc_max3) && (DC_r1 > Min_r1) && (DC_r2 > Min_r2) && (DC_r3 > Min_r3));
-}
+//         // particle must be within all fiducial bands in all 3 regions and pass inner circular cut
+//         return ((Y1 > calc_min1) && (Y1 < calc_max1) && (Y2 > calc_min2) && (Y2 < calc_max2) && (Y3 > calc_min3) &&
+//                 (Y3 < calc_max3) && (DC_r1 > Min_r1) && (DC_r2 > Min_r2) && (DC_r3 > Min_r3));
+// }
 
 bool Pass2_Cuts::EC_sampling_fraction_cut()
 {
